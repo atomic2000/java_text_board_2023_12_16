b6f7a089bf3be0423033a709060e3420c407083c
atomic2000 <atomic2000@naver.com>
2023-12-16 11:03:20 +0900

    게시물/equals/scanner/while/if

--- src/main/java/org/sbs/exam/board/Main.java
+++ src/main/java/org/sbs/exam/board/Main.java
@@ -6,26 +6,28 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    while (true) {
-      System.out.printf("명령 ) ");
-      String cmd1 = sc.nextLine();
-      System.out.println("== 게시물 등록 ==");
-      System.out.printf("제목 : ");
-      String cmd2 = sc.nextLine();
-      System.out.printf("내용 : ");
-      String cmd3 = sc.nextLine();
-      System.out.println("1번 게시물이 등록 되었습니다.");
-      System.out.println(" ");
-    }
-    /*
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 게시판 시작 ==");
 
+    while (true) {
+      System.out.printf("명령 ) ");
+      String cmd = sc.nextLine();
 
-    System.out.printf("명령 )");
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", cmd);
-    */
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
 
+        int id = 1;
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      }
+      else if (cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+    }
   }
 }
